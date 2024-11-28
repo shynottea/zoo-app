599f0b1dd6763361cc6fc1584bed3a428532d49a
Islam <kogsaitama@gmail.com>
2024-11-28 18:03:26 +0500

    app js resolve

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Navbar from './Navbar';
 import Cart from './routes/Cart/Cart';
 import ProductList from './routes/Product/ProductList';
 import ProductDetails from './routes/Product/ProductDetails';
-import ProductLayout from './routes/Product/ProductLayout';
 import Contacts from './routes/Contacts';
 import Login from './routes/Authentication/Login';
 
@@ -34,7 +33,6 @@ const App = () => {
     }
   }, [location.pathname]); 
   return (
-<<<<<<< HEAD
     <Layout>
       <Header>
         <div className="demo-logo" />
@@ -59,40 +57,6 @@ const App = () => {
         </Content>
       </Layout>
     </Layout>
-=======
-    <AuthProvider>
-      <CartProvider>
-        <Layout>
-          <Header>
-            <div className="demo-logo" />
-            <Navbar onSearch={handleSearch} />
-          </Header>
-          <Layout>
-            <Content
-              style={{
-                padding: 24,
-                margin: 0,
-                minHeight: 280,
-              }}
-            >
-              <Routes>
-                {/*<Route path="/productlist" element={<ProductList searchQuery={searchQuery} />} />*/}
-                {/*<Route path="/products/:id" element={<ProductDetails />} />*/}
-                <Route path="/products" element={<ProductLayout />}>
-                  <Route index element={<ProductList searchQuery={searchQuery} />} />
-                  <Route path=":id" element={<ProductDetails />} />
-                </Route>
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/contacts" element={<Contacts />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/" element={<Navigate to="/products" />} />
-              </Routes>
-            </Content>
-          </Layout>
-        </Layout>
-      </CartProvider>
-    </AuthProvider>
->>>>>>> 3b061df766d5bcae320042e3f20d74782572eeb6
   );
 };
 
